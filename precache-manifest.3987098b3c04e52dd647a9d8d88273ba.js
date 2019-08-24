db0317fd78005d975f0a4ea71f4a94ee4ca2d060
talitast <talitatravassos.tt@gmail.com>
2019-08-24 11:27:02 -0300

    Updates

---
+++ precache-manifest.3987098b3c04e52dd647a9d8d88273ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c79365c3ffbc7f0aea2",
+    "url": "/swapi-react-app/static/js/runtime~main.0c79365c.js"
+  },
+  {
+    "revision": "ef4c779bc05e58a95908",
+    "url": "/swapi-react-app/static/js/main.ef4c779b.chunk.js"
+  },
+  {
+    "revision": "09e51ed4ebd052b5ef54",
+    "url": "/swapi-react-app/static/js/1.09e51ed4.chunk.js"
+  },
+  {
+    "revision": "ef4c779bc05e58a95908",
+    "url": "/swapi-react-app/static/css/main.7e2f50a6.chunk.css"
+  },
+  {
+    "revision": "09e51ed4ebd052b5ef54",
+    "url": "/swapi-react-app/static/css/1.6a4cb39a.chunk.css"
+  },
+  {
+    "revision": "1884e5a5956bbb073278a57cbabc2cc2",
+    "url": "/swapi-react-app/index.html"
+  }
+];
